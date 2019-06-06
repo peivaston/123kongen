db57f39426679b6de5ce071c1dfee81558c5fcdc
peivaston <peivaston@hotmail.com>
2019-06-06 11:22:46 +0200

    third commit

---
+++ impEv.js
@@ -0,0 +1,7 @@
+function openImpEv() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeImpEv() {
+    document.getElementById("myForm").style.display = "none";
+  }
